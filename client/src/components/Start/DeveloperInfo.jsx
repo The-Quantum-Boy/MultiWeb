12593a02c4afcd7dbc6a90667ba1926b9ff142f5
$umit Dhanorkar <101516983+The-Quantum-Boy@users.noreply.github.com>
2024-01-01 22:41:29 +0530

    Update DeveloperInfo.jsx

--- client/src/components/Start/DeveloperInfo.jsx
+++ client/src/components/Start/DeveloperInfo.jsx
@@ -1,32 +1,7 @@
 import React from 'react'
 import logo from '../../images/assets/logo-dark.png'
 const DeveloperInfo = () => {
-  return (
-    <div className='developer_info'>
-    <center >
-      <img src={logo} alt="" srcset="" style={{
-        width:'100px'
-      }}/>
-      <h3>
-        Government College of Engineering Amravati
-      </h3>
-    </center>
-    <center>
-      <h6 style={{
-        fontWeight:'bold'
-      }}>Guided By</h6>
-      <p style={{
-        fontWeight:'bold'
-      }}>Prof. K. N. Tayde</p>
-    </center>
-    <center>
-      <h6>Submitted By</h6>
-      {/* <p>Tanvi Raut (19007073)</p>
-      <p>Harshu Amule (19007072)</p>
-      <p>Akshay Khare (19003058)</p>
-      <p>Gaurav Bhagat (19007069)</p> */}
-    </center>
-    </div>
+  return (<></>
   )
 }
 
